3f3b23c27c92ce048396e89d7bba2836db55bc93
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-01-23 14:12:56 +1100

    Add readChar, newToken, and nextToken methods and functions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,14 +1,67 @@
 // Package lexer -
 package lexer
 
+import (
+	"unicode"
+
+	"github.com/shanehowearth/interpreter/token"
+)
+
+// Lexer -
 type Lexer struct {
-	input        string
+	input        []rune
 	position     int  // current position in input (points to current char)
 	readPosition int  // current reading position in input (after current char)
-	ch           byte // current char under examination
+	ch           rune // current char under examination
 }
 
+// New -
 func New(input string) *Lexer {
-	l := &Lexer{input: input}
+	l := &Lexer{input: []rune(input)}
+	l.readChar()
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		// Nothing read yet, or EOF
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+// NextToken -
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	default:
+		tok = newToken(token.ILLEGAL, l.ch)
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch rune) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -9,32 +9,99 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `=+(){},;`
 
-	tests := map[string]struct {
+	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
-		"Assign":    {token.ASSIGN, "="},
-		"Plus":      {token.PLUS, "+"},
-		"LParen":    {token.LPAREN, "("},
-		"RParen":    {token.RPAREN, ")"},
-		"LBrace":    {token.LBRACE, "{"},
-		"RBrace":    {token.RBRACE, "}"},
-		"Comma":     {token.COMMA, ","},
-		"Semicolon": {token.SEMICOLON, ";"},
-		"EOF":       {token.EOF, ""},
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		// {token.EOF, string('0')},
 	}
 
 	l := New(input)
-
 	for i, tt := range tests {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("%s - TokenType wrong, expected=%q, got=%q", i, tt.expectedType, tok.Type)
+			t.Fatalf("%d - TokenType wrong, expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("%s - Literal wrong, expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+			t.Fatalf("%d - Literal wrong, expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestNextTokenLongerInput(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+
+};
+
+let result = add(five, ten);
+`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		// {token.EOF, string('0')},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("%d - TokenType wrong, expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("%d - Literal wrong, expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
