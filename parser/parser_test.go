a41b7556dd5b1fd7fc97791e8a7528039f6c4bb8
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-06-28 20:19:03 +1000

    Add call expressions

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -248,6 +248,9 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 		"grouped 03":       {"2 / (5 + 5)", "(2 / (5 + 5))"},
 		"grouped 04":       {"-(5 + 5)", "(-(5 + 5))"},
 		"grouped 05":       {"!(true == true)", "(!(true == true))"},
+		"call function 01": {"a + add(b * c) + d", "((a + add((b * c))) + d)"},
+		"call function 02": {"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))", "add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))"},
+		"call function 03": {"add(a + b + c * d / f + g)", "add((((a + b) + ((c * d) / f)) + g))"},
 	}
 
 	for name, tt := range tests {
@@ -430,4 +433,35 @@ func TestFunctionParameterParsing(t *testing.T) {
 			testLiteralExpression(t, function.Parameters[i], ident)
 		}
 	}
+}
+
+func TestCallExpressionParsing(t *testing.T) {
+	input := "add(1, 2 * 3, 4 + 5);"
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+			1, len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("stmt is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+	exp, ok := stmt.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.CallExpression. got=%T",
+			stmt.Expression)
+	}
+	if !testIdentifier(t, exp.Function, "add") {
+		return
+	}
+	if len(exp.Arguments) != 3 {
+		t.Fatalf("wrong length of arguments. got=%d", len(exp.Arguments))
+	}
+	testLiteralExpression(t, exp.Arguments[0], 1)
+	testInfixExpression(t, exp.Arguments[1], 2, "*", 3)
+	testInfixExpression(t, exp.Arguments[2], 4, "+", 5)
 }
