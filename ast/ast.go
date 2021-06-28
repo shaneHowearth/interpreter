0e34f4d51c9562fe88348bb82c0c08663c4860e1
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-06-28 19:29:37 +1000

    Add function literals

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/shanehowearth/interpreter/token"
 )
@@ -275,5 +276,34 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+	return out.String()
+}
+
+// FunctionLiteral -
+type FunctionLiteral struct {
+	Token      token.Token // the { token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+
+// TokenLiteral -
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+
+// String -
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
 	return out.String()
 }
