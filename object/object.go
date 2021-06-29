24efb954f5ce04c12c75519f19a91803f2c93961
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-06-29 13:12:00 +1000

    Add integer objects

--- object/object.go
+++ object/object.go
@@ -1,13 +1,32 @@
 // Package object -
 package object
 
+import "fmt"
+
 // ObjectType -
 // ignore stutter warning
 // nolint: revive
 type ObjectType string
 
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
 // Object -
 type Object interface {
 	Type() ObjectType
 	Inspect() string
+}
+
+// Integer -
+type Integer struct {
+	Value int64
+}
+
+// Inspect -
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+// Type -
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
 }
