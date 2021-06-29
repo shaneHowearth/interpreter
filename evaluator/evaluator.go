10c24a321ba9946209c7ef7be17236910502bbf0
Shane Howearth <Shane.Howearth@shanehowearth.com>
2021-06-29 18:25:30 +1000

    Evaluate booleans

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,6 +6,11 @@ import (
 	"github.com/shanehowearth/interpreter/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 // Eval -
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -14,9 +19,11 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
-		// Expressions
+	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -27,4 +34,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
